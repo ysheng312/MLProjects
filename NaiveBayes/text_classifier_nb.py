62f21bfe4382a8448295fed45cb947cdfe4b04cc
josheng <aachen312@gmail.com>
2018-10-14 14:24:16 -0600

    email spam classifier

--- NaiveBayes/text_classifier_nb.py
+++ NaiveBayes/text_classifier_nb.py
@@ -78,6 +78,48 @@ def testing_nb():
     print(test_entry, 'classified as: ', classify_nb(this_doc, p0_v, p1_v, p_ab))
 
 
+def text_parse(big_string):
+    import re
+    list_of_tokens = re.split(r'\W*', big_string)
+    return [tok.lower() for tok in list_of_tokens if len(tok) > 2]
+
+
+def spam_test():
+    doc_list = []
+    class_list = []
+    full_text = []
+    for i in range(1, 26):
+        word_list = text_parse(open('path/file.txt').read())
+        doc_list.append(word_list)
+        full_text.extend(word_list)
+        class_list.append(1)
+        word_list = text_parse(open('path/file.txt').read())
+        doc_list.append(word_list)
+        full_text.extend(word_list)
+        class_list.append(0)
+
+    vocab_list = create_vocab_list(doc_list)
+    training_set = range(50)
+    test_set = []
+    for i in range(10):
+        rand_index = int(random.uniform(0, len(training_set)))
+        test_set.append(training_set[rand_index])
+        del(training_set[rand_index])
+    train_mat = []
+    train_classes = []
+    for doc_index in training_set:
+        train_mat.append(bag_of_words_2_vec_mn(vocab_list, doc_list[doc_index]))
+        train_classes.append(class_list[doc_index])
+    p0_v, p1_v, p_spam = train_nb_0(array(train_mat), array(train_classes))
+    error_count = 0
+
+    for doc_index in test_set:
+        word_vector = bag_of_words_2_vec_mn(vocab_list, doc_list[doc_index])
+        if classify_nb(array(word_vector), p0_v, p1_v, p_spam) != class_list[doc_index]:
+            error_count += 1
+    print('the error rate is: ', float(error_count)/len(test_set))
+
+
 def main():
     testing_nb()
 
